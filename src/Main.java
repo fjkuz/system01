f3abc75f7a6bac5be99e3b2d62b390bda184e60d
fjkuuz <fjkuz@icloud.com>
2026-09-06 16:26:14 +0300

    /

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.util.Scanner;
+public class Main {
+    void main() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Расчет a * b \na = ");
+        int a = scan.nextInt();
+        System.out.print("b = ");
+        int b = scan.nextInt();
+        System.out.print("a * b = ");
+        System.out.print(mult(a,b));
+    }
+    static int mult(int a, int b){
+        int rez=0;
+        for(int i=0;i<b;i++){
+            rez+=a;
+        }
+        return rez;
+    }
 }
