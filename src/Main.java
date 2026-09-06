702e9a98de03dcd27d759edb5a932376f2ff3ee7
fjkuuz <fjkuz@icloud.com>
2026-09-06 16:29:01 +0300

    /

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
-public class Main {
-    void main() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Расчет a * b \na = ");
-        int a = scan.nextInt();
-        System.out.print("b = ");
-        int b = scan.nextInt();
-        System.out.print("a * b = ");
-        System.out.print(mult(a,b));
+void main() {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Расчет a * b \na = ");
+    int a = scan.nextInt();
+    System.out.print("b = ");
+    int b = scan.nextInt();
+    System.out.print("a * b = ");
+    System.out.print(mult(a,b));
+}
+static int mult(int a, int b){
+    int rez=0;
+    for(int i=0;i<b;i++){
+        rez+=a;
     }
-    static int mult(int a, int b){
-        int rez=0;
-        for(int i=0;i<b;i++){
-            rez+=a;
-        }
-        return rez;
-    }
+    return rez;
 }
