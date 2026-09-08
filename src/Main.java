9e28e5f0b402fe6ac77f1ce922f0d00a4a128f48
fjkuuz <fjkuz@icloud.com>
2026-09-08 10:22:36 +0300

    //

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,46 @@ void main() {
     int a = scan.nextInt();
     System.out.print("b = ");
     int b = scan.nextInt();
-    System.out.print("a * b = ");
-    System.out.print(mult(a,b));
+    System.out.print("Способ 1\n a * b = ");
+    System.out.println(mult1(a,b));
+    System.out.print("Способ 2\n a * b = ");
+    System.out.println(mult2(a,b));
+    System.out.print("Способ 3\n a * b = ");
+    System.out.println(mult3(a,b));
+    System.out.print("Способ 4\n a * b = ");
+    System.out.print(mult4(a,b));
 }
-static int mult(int a, int b){
+static int mult1(int a, int b){
     int rez=0;
     for(int i=0;i<b;i++){
         rez+=a;
+    }
+    return rez;
+}
+static int mult2(int a, int b){
+    int rez=0;
+    while(b>0){
+        rez+=a;
+        b--;
+    }
+    return rez;
+}
+static int mult3(int a, int b){
+    if (b == 0) {
+        return 0;
+    }
+    else{
+        return a +mult3(a,b-1);
+    }
+}
+static int mult4(int a, int b){
+    int rez=0;
+    while(b>0){
+        if(b%2!=0) {
+            rez += a;
+        }
+        a+=a;
+        b/=2;
     }
     return rez;
 }
