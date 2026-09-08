fb88ce6319b5e1fb2e17cd000cdac9343cd15a57
fjkuuz <fjkuz@icloud.com>
2026-09-08 20:51:24 +0300

    //.

--- src/Main.java
+++ src/Main.java
@@ -12,24 +12,23 @@ void main() {
     System.out.print("Способ 3\n a * b = ");
     System.out.println(mult3(a,b));
     System.out.print("Способ 4\n a * b = ");
-    System.out.print(mult4(a,b));
+    System.out.println(mult4(a,b));
+    System.out.print("Способ 5\n a * b = ");
+    System.out.println(mult5(a,b));
+    System.out.print("Способ 6\n a * b = ");
+    System.out.println(mult6(a,b));
 }
-static int mult1(int a, int b){
+static int mult1(int a, int b){//сложение
     int rez=0;
     for(int i=0;i<b;i++){
         rez+=a;
     }
     return rez;
 }
-static int mult2(int a, int b){
-    int rez=0;
-    while(b>0){
-        rez+=a;
-        b--;
-    }
-    return rez;
+static int mult2(int a, int b){//логарифм
+ return (int)Math.round(Math.exp(Math.log(a)+Math.log(b)));
 }
-static int mult3(int a, int b){
+static int mult3(int a, int b){//рекурсия
     if (b == 0) {
         return 0;
     }
@@ -37,14 +36,24 @@ static int mult3(int a, int b){
         return a +mult3(a,b-1);
     }
 }
-static int mult4(int a, int b){
+static int mult4(int a, int b){//сложение с проверкой переполнения
     int rez=0;
-    while(b>0){
-        if(b%2!=0) {
-            rez += a;
-        }
-        a+=a;
-        b/=2;
+    for(int i=0;i<b;i++){
+        rez=Math.addExact(rez,a);
     }
     return rez;
+}
+static int mult5(int a, int b){//битовое сложение
+    int rez=0;
+    while (b != 0) {
+        if((b&1)==1){
+            rez+=a;
+        }
+        a<<=1;
+        b>>=1;
+    }
+    return rez;
+}
+static int mult6(int a,int b){
+    return(int)Math.round(Math.pow(10,Math.log10(a)+Math.log10(b)));
 }
